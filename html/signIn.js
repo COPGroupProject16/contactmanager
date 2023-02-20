40594d64545c44056e64850728447e92c59a6644
JPacheco35 <99419989+JPacheco35@users.noreply.github.com>
2023-02-20 17:00:25 -0500

    Added Comments of Signin.js

--- html/signIn.js
+++ html/signIn.js
@@ -1,72 +1,88 @@
-// Login Function()
+// Login Function
 function doLogin()
 {
+	// Cookie Values to be filled in on sucessfull login
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
-	let user = document.getElementById("username").value;
-	let pass = document.getElementById("password").value;
 
 	// Make Sure Input Fields are Valid (Not Empty) Return if missing input
-   	var form = document.getElementById('loginForm');
-   	if(form.reportValidity() == false) { return; }
+	var form = document.getElementById('loginForm');
+	if(form.reportValidity() == false) { return; }
+	
+	// Grab User Input
+	let user = document.getElementById("username").value;
+	let pass = document.getElementById("password").value;
 
 	// Hash Password
 	var hash = md5(pass);
 	hash = hash.substr(0, 25); // Only Grab 1st 25 Characters
-	//console.log(hash);
 
-	//let jsonPayload = JSON.stringify({ username: user, password: pass });
+	// Convert Input to JSON
 	let jsonPayload = JSON.stringify({username:user,password:hash});
 	console.log(jsonPayload);
 	
+	// Get [Current URL]/login.php PATH
 	let url = location.href.substring(0, location.href.lastIndexOf("/")+1) + '/login.php';
  
-
+	// Open XML Request to SQL Database
 	let xhr = new XMLHttpRequest();
   	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
  
 	try
 	{
+		// When we have recieved a response, do this
 		xhr.onreadystatechange = function() 
 		{
+			// Parse Repose Text for a User ID
 			let jsonObject = JSON.parse(xhr.responseText);
 			userId = jsonObject.id;
 
+			// Check if there actually was a response
 			if (this.readyState == 4 && this.status == 200) 
 			{
+				// User ID < 1 ---> USER DOES NOT EXIST
 				if( userId < 1 )
 				{		                    
-          			// Blink Effect 
+          			// Blink Invalid Credentials Effect 
  			    	setTimeout(function(){document.getElementById("loginResult1").innerHTML = "Username/Password Combination Incorrect";},250);   
 					document.getElementById("loginResult1").innerHTML = " ";
 					return;
 				}
+
+				// If we reach here, we have a successfull login
 		
+				// Save Cookie Detials
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
-
         		saveCookie();
+
+				// Move to Main Webpage
 				window.location.href = "main.html";
 			}
 		};
-		xhr.send(jsonPayload); // THE 
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult1").innerHTML = err.message;
+
+		// Send JSON to MySQL server
+		xhr.send(jsonPayload);
 	}
 
+	// We've encountered some error
+	catch(err) { document.getElementById("loginResult1").innerHTML = err.message; }
 }
 
-// Signup Function()
+
+// Signup Function --> Bascially Identical to Login, but add User if NOT Found, return error if Found
 function doSignup()
 {
+	// Cookie Values to be filled in on sucessfull account creation
 	userId = 0;
 	firstName = "";
 	lastName = "";
+
+	// Make Sure Input Fields are Valid (Not Empty)
+	var form = document.getElementById('signupForm');
+	if(form.reportValidity() == false) { return; }
 
 	// Grab User Input
 	let fName = document.getElementById("firstnameCreate").value;
@@ -74,10 +90,6 @@ function doSignup()
 	let user = document.getElementById("usernameCreate").value;
 	let pass = document.getElementById("passwordCreate").value;
 	let passCheck = document.getElementById("passwordcheckCreate").value;
-
-	// Make Sure Input Fields are Valid (Not Empty)
-	var form = document.getElementById('signupForm');
-	if(form.reportValidity() == false) { return; }
 
 	// Make Sure Passwords Match
 	if(pass != passCheck)
@@ -88,70 +100,85 @@ function doSignup()
 
 	// Hash Password
 	var hash = md5( pass );
-  	hash = hash.substr(0, 25);
+  	hash = hash.substr(0, 25); // Grab 1st 25 Characters
 
 	// Stringify Input
-	//let jsonPayload = JSON.stringify({ username: user, password: pass, firstName: fName, lastName: lName });
 	let jsonPayload = JSON.stringify({ username: user, password: hash, firstName: fName, lastName: lName });
-	console.log(jsonPayload);
-
  
-	// Get Proper URL
+	// Get [Current URL]/signup.php PATH
 	let url = location.href.substring(0, location.href.lastIndexOf("/")+1) + '/signup.php';
  
+	// Open XML Request to SQL Database
 	let xhr = new XMLHttpRequest();
   	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
  
 	try
 	{
+		// When we have recieved a response, do this
 		xhr.onreadystatechange = function() 
 		{
+			// Parse Repose Text for a User ID
 			let jsonObject = JSON.parse(xhr.responseText);
 			userId = jsonObject.id;
 
+			// Check if there actually was a response
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				// User already exists --> Return Error
 				if(userId < 1)
 				{		 
-         		 	// Blink Effect 
+         		 	// Blink Effect User Already Taken
 					  setTimeout(function(){document.getElementById("loginResult2").innerHTML = "Username Already Taken";},250);   
 					  document.getElementById("loginResult2").innerHTML = " ";
 					  return;              
 				}
 				
-				// User Does Not Exist --> Create User
+				// User Does NOT Exist --> Create User Account
 				else
 				{
+					// Save Cookie Details
 					firstName = jsonObject.firstName;
 					lastName = jsonObject.lastName;
-	
 					saveCookie();
+
+					// Move to Main Webpage
 					window.location.href = "main.html";        
 				}
-			}
+			} 
 		};
+
+		// Send JSON to MySQL server
 		xhr.send(jsonPayload);
 	}
-	catch(err)
-	{
-		document.getElementById("loginResult2").innerHTML = err.message;
-	}
+
+	// We've encountered some error
+	catch(err) { document.getElementById("loginResult2").innerHTML = err.message; }
 
 }
 
 // Show Password Field on Login/Sign Up Page(s)
 function showPass() 
 {
+	// If password, change to text
+	// If text, change to password
+
+	// Password Field on Login
     var x = document.getElementById("password");
 	
     if (x.type === "password") { x.type = "text"; } 
 	else { x.type = "password"; }
 
-    x = document.getElementById("passwordcheck");
+	// Password Field on Sign Up
+    x = document.getElementById("passwordCreate");
 
     if (x.type === "password") { x.type = "text"; }
+	else { x.type = "password"; }
+
+	// Password Check Field on Sign Up
+	x = document.getElementById("passwordcheckCreate");
+
+	if (x.type === "password") { x.type = "text"; }
 	else { x.type = "password"; }
 }
 
@@ -161,18 +188,20 @@ function saveCookie()
 	let minutes = 30;
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
+
+	// All Cookies Expire 30 Minutes from when this function is called
 	document.cookie = "firstName=" + firstName + ";" + "expires=" + date.toGMTString();
 	document.cookie = "lastName=" + lastName + ";" + "expires=" + date.toGMTString();
 	document.cookie = "userId=" + userId + ";" + "expires=" + date.toGMTString();
-  	//console.log(document.cookie);
 }
 
 // Read in Cookie Details
 function readCookie()
 {
 	let data = document.cookie;
-	let splits = data.split(";");
 
+	// Split Up the Cookie into each of its values and Parse them
+	let splits = data.split(";");
 	for(var i = 0; i < splits.length; i++) 
 	{
 		let thisOne = splits[i].trim();
@@ -192,13 +221,13 @@ function readCookie()
 		}
 	}
 
-    // If there is no cookie AKA no user is signed in --> go to login page (index.html)
-	//if( userId < 0 ) { window.location.href = "index.html";}
+    // If there is NO cookie AKA no user is signed in --> redirect to login/signup page (index.html)
 	if (document.cookie.indexOf('userId') == -1 ) { window.location.href = "index.html"; }
 
     // If there IS a saved user cookie --> go to home page (main.html)
     else 
 	{ 
+		// Fill Hello User Banner with cookie details
 		document.getElementById("helloBanner1").innerText = "Hello,  " + firstName + " " + lastName;
 		document.getElementById("helloBanner2").innerText = "Hello,  " + firstName + " " + lastName; 
 		document.getElementById("userID").innerText = userId;
@@ -220,20 +249,14 @@ function doLogout()
 	// Then throw in this as a filler 
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 
-    // Go pack to login page (index.html)
+    // Redirect to login page (index.html)
 	window.location.href = "index.html";
 }		
 
 // addContact Function on main.html
 function addContact(formName)
 {
-	// Get User Input
-	//let firstname = document.querySelector("#" + formName + " [name=firstname]").value;
-	//let lastname = document.querySelector("#" + formName + " [name=lastname]").value;
-	//let email = document.querySelector("#" + formName + " [name=email]").value;
-	//let phonenum = document.querySelector("#" + formName + " [name=phonenum]").value;
-	//console.log(formName);
-
+	// Placeholders for data fields
 	let firstname = "";
 	let lastname = "";
 	let email = "";
@@ -245,6 +268,7 @@ function addContact(formName)
 		// Check that all fields are present
 		if(document.getElementById(formName).reportValidity() == false) { return; }
 
+		// Get User Input
 		firstname = document.getElementById("firstname-m").value;
 		lastname = document.getElementById("lastname-m").value;
 		email = document.getElementById("email-m").value;
@@ -254,8 +278,10 @@ function addContact(formName)
 	// Addd Contact from Desktop Version
 	else
 	{
+		// Check that all fields are present
 		if(document.getElementById(formName).reportValidity() == false) { return; }
 
+		// Get User Input
 		firstname = document.getElementById("firstname").value;
 		lastname = document.getElementById("lastname").value;
 		email = document.getElementById("email").value;
@@ -270,32 +296,33 @@ function addContact(formName)
 	var dd = String(today.getDate()).padStart(2, '0');
 	var mm = String(today.getMonth() + 1).padStart(2, '0');
 	var yyyy = today.getFullYear();
-	
 	today = mm + '/' + dd + '/' + yyyy;
-	//console.log(today);
 
-	// Filler if no Email or Phone Number Entered
+	// Filler if no Email or Phone Number Entered -- They are not required
 	if (email == "" || email.trim().length == 0) { email = "-"; }
 	if (phonenum == "" || phonenum.trim().length == 0) { email = "-"; }
 
 	// Stringify Input
 	let jsonPayload = JSON.stringify({firstname: firstname, lastname: lastname, email: email, phonenum: phonenum, userId: userId, adddate: today});
-	//console.log(jsonPayload);
 
 	// Get Proper URL
 	let url = location.href.substring(0, location.href.lastIndexOf("/")+1) + '/addContact.php';
 
+	// Open XML Request to SQL Database
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	
 	try
 	{
+		// When we have recieved a response, do this
 		xhr.onreadystatechange = function() 
 		{
+			// Get and Parse Response Text
 			let jsonObject = JSON.parse(xhr.responseText);
 			userId = jsonObject.id;
 
+			// If Valid Server Response --- THIS STILL NEEDS REVIEW
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				// User already exists --> Return Error
@@ -314,10 +341,11 @@ function addContact(formName)
 				}
 			}
 		};
+
+		// Send JSON to MySQL database
 		xhr.send(jsonPayload);
 	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+
+	// We've encounted an error here
+	catch(err) { document.getElementById("loginResult").innerHTML = err.message; }
 }
